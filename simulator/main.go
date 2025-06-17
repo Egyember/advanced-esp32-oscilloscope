3953c8fd81d0b82f9ea05da7072479b3c60ee436
Egyember <gyengezsombor2005@gmail.com>
2025-06-17 21:06:24 +0200

    fixes

--- simulator/main.go
+++ simulator/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net"
 	"time"
 )
+
+type config struct{
+	channels uint8;
+	sampleRate uint32; // expected ADC sampling frequency in Hz.
+	duration uint32;   // in ms
+};
+
+func parseConf(buf []byte) (conf config, err error) {
+	err = nil
+	if len(buf) <9 {
+		err =  errors.New("to short buffer")
+		return
+	}
+	conf.channels = buf[0]
+	return
+}
 
 func advertise(what string){
 	con, err := net.Dial("udp", "255.255.255.255:40000")
@@ -11,11 +29,33 @@ func advertise(what string){
 		panic(err)
 	}
 	for{
-		con.Write([]byte(what))
+		_, err = con.Write([]byte(what))
+		if err != nil {
+			panic(err)
+		}
 		time.Sleep(5 * time.Second)
 	};
 };
 func main(){
-	advertise("oscilloscope here")
+	go advertise("oscilloscope here")
+	lisen, err := net.Listen("tcp4", "0.0.0.0:40001")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		con, err := lisen.Accept()
+		if err != nil {
+			panic(err)
+		}
+		readbuff := make([]byte, 9, 9)
+		con.Read(readbuff)
+		conf, err := parseConf(readbuff)
+		if err != nil {
+			con.Close()
+			panic(err)
+		}
+		fmt.Println(conf)
+		con.Close()
+	}
 
 }
