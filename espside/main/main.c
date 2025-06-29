9b8da7a7f18974c035d2d16bfc367f8270e9d696
Egy_ember <kamufiok06@gmail.com>
2025-06-30 01:40:23 +0200

    adc hacks to set the pattern table correctly

--- espside/main/main.c
+++ espside/main/main.c
@@ -26,6 +26,7 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -65,6 +66,132 @@ struct localIp {
  -1 if no connection avalable
  */
 struct localIp *localIp;
+
+//adc woodoo
+// copied from componentes/driver/deprecated/adc_i2s_deprecated.c
+#include "sdkconfig.h"
+#include "esp_types.h"
+#include "esp_log.h"
+#include "esp_intr_alloc.h"
+#include "driver/rtc_io.h"
+#include "freertos/FreeRTOS.h"
+#include "hal/adc_ll.h"
+#include "hal/adc_types.h"
+extern portMUX_TYPE rtc_spinlock; //TODO: Will be placed in the appropriate position after the rtc module is finished.
+#define ADC_ENTER_CRITICAL()  portENTER_CRITICAL(&rtc_spinlock)
+#define ADC_EXIT_CRITICAL()  portEXIT_CRITICAL(&rtc_spinlock)
+
+
+typedef struct {
+    union {
+        struct {
+            uint8_t atten:     2;   /*!< ADC sampling voltage attenuation configuration. Modification of attenuation affects the range of measurements.
+                                         0: measurement range 0 - 800mV,
+                                         1: measurement range 0 - 1100mV,
+                                         2: measurement range 0 - 1350mV,
+                                         3: measurement range 0 - 2600mV. */
+            uint8_t bit_width: 2;   /*!< ADC resolution.
+-                                         0: 9 bit;
+-                                         1: 10 bit;
+-                                         2: 11 bit;
+-                                         3: 12 bit. */
+            int8_t channel:   4;   /*!< ADC channel index. */
+        };
+        uint8_t val;                /*!<Raw data value */
+    };
+} adc_digi_pattern_table_t;
+
+
+typedef enum {
+    ADC_DIGI_FORMAT_12BIT,  /*!<ADC to DMA data format,                [15:12]-channel, [11: 0]-12 bits ADC data (`adc_digi_output_data_t`). Note: For single convert mode. */
+    ADC_DIGI_FORMAT_11BIT,  /*!<ADC to DMA data format, [15]-adc unit, [14:11]-channel, [10: 0]-11 bits ADC data (`adc_digi_output_data_t`). Note: For multi or alter convert mode. */
+    ADC_DIGI_FORMAT_MAX,
+} adc_digi_format_t;
+typedef struct {
+    uint32_t adc1_pattern_len;               /*!<Pattern table length for digital controller. Range: 0 ~ 16 (0: Don't change the pattern table setting).
+                                                 The pattern table that defines the conversion rules for each SAR ADC. Each table has 16 items, in which channel selection,
+                                                 resolution and attenuation are stored. When the conversion is started, the controller reads conversion rules from the
+                                                 pattern table one by one. For each controller the scan sequence has at most 16 different rules before repeating itself. */
+    uint32_t adc2_pattern_len;               /*!<Refer to ``adc1_pattern_len`` */
+    adc_digi_pattern_table_t *adc1_pattern;  /*!<Pointer to pattern table for digital controller. The table size defined by `adc1_pattern_len`. */
+    adc_digi_pattern_table_t *adc2_pattern;  /*!<Refer to `adc1_pattern` */
+    adc_digi_convert_mode_t conv_mode;       /*!<ADC conversion mode for digital controller. See ``adc_digi_convert_mode_t``. */
+    adc_digi_format_t format;                /*!<ADC output data format for digital controller. See ``adc_digi_format_t``. */
+} adc_digi_config_t;
+
+#define DIG_ADC_OUTPUT_FORMAT_DEFUALT (ADC_DIGI_FORMAT_12BIT)
+#define DIG_ADC_ATTEN_DEFUALT         (ADC_ATTEN_DB_12)
+#define DIG_ADC_BIT_WIDTH_DEFUALT     (3)   //3 for ADC_WIDTH_BIT_12
+extern esp_err_t adc_common_gpio_init(adc_unit_t adc_unit, adc_channel_t channel);
+
+static inline void adc_ll_digi_prepare_pattern_table(adc_unit_t adc_n, uint32_t pattern_index, adc_digi_pattern_table_t pattern)
+{
+    uint32_t tab;
+    uint8_t index = pattern_index / 4;
+    uint8_t offset = (pattern_index % 4) * 8;
+    if (adc_n == ADC_UNIT_1) {
+        tab = SYSCON.saradc_sar1_patt_tab[index];   // Read old register value
+        tab &= (~(0xFF000000 >> offset));           // clear old data
+        tab |= ((uint32_t)pattern.val << 24) >> offset; // Fill in the new data
+        SYSCON.saradc_sar1_patt_tab[index] = tab;   // Write back
+    } else { // adc_n == ADC_UNIT_2
+        tab = SYSCON.saradc_sar2_patt_tab[index];   // Read old register value
+        tab &= (~(0xFF000000 >> offset));           // clear old data
+        tab |= ((uint32_t)pattern.val << 24) >> offset; // Fill in the new data
+        SYSCON.saradc_sar2_patt_tab[index] = tab;   // Write back
+    }
+}
+
+static inline void adc_ll_digi_set_output_format(bool data_sel)
+{
+    SYSCON.saradc_ctrl.data_sar_sel = data_sel;
+}
+
+static void adc_digi_controller_reg_set(const adc_digi_config_t *cfg)
+{
+    /* On ESP32, only support ADC1 */
+    switch (cfg->conv_mode) {
+    case ADC_CONV_SINGLE_UNIT_1:
+        adc_ll_digi_set_convert_mode(ADC_LL_DIGI_CONV_ONLY_ADC1);
+        break;
+    case ADC_CONV_SINGLE_UNIT_2:
+        adc_ll_digi_set_convert_mode(ADC_LL_DIGI_CONV_ONLY_ADC2);
+        break;
+    case ADC_CONV_BOTH_UNIT:
+        adc_ll_digi_set_convert_mode(ADC_LL_DIGI_CONV_BOTH_UNIT);
+        break;
+    case ADC_CONV_ALTER_UNIT:
+        adc_ll_digi_set_convert_mode(ADC_LL_DIGI_CONV_ALTER_UNIT);
+        break;
+    default:
+        abort();
+    }
+
+    if (cfg->conv_mode & ADC_CONV_SINGLE_UNIT_1) {
+        adc_ll_set_controller(ADC_UNIT_1, ADC_LL_CTRL_DIG);
+        if (cfg->adc1_pattern_len) {
+            adc_ll_digi_clear_pattern_table(ADC_UNIT_1);
+            adc_ll_digi_set_pattern_table_len(ADC_UNIT_1, cfg->adc1_pattern_len);
+            for (uint32_t i = 0; i < cfg->adc1_pattern_len; i++) {
+                adc_ll_digi_prepare_pattern_table(ADC_UNIT_1, i, cfg->adc1_pattern[i]);
+            }
+        }
+    }
+    if (cfg->conv_mode & ADC_CONV_SINGLE_UNIT_2) {
+        adc_ll_set_controller(ADC_UNIT_2, ADC_LL_CTRL_DIG);
+        if (cfg->adc2_pattern_len) {
+            adc_ll_digi_clear_pattern_table(ADC_UNIT_2);
+            adc_ll_digi_set_pattern_table_len(ADC_UNIT_2, cfg->adc2_pattern_len);
+            for (uint32_t i = 0; i < cfg->adc2_pattern_len; i++) {
+                adc_ll_digi_prepare_pattern_table(ADC_UNIT_2, i, cfg->adc2_pattern[i]);
+            }
+        }
+    }
+    adc_ll_digi_set_output_format(cfg->format);
+    adc_ll_digi_convert_limit_enable(ADC_LL_DEFAULT_CONV_LIMIT_EN);
+    adc_ll_digi_set_convert_limit_num(ADC_LL_DEFAULT_CONV_LIMIT_NUM);
+    //adc_ll_digi_set_data_source(ADC_I2S_DATA_SRC_ADC); //where the fuck the macro com from?
+}
 
 int retry = 0;
 void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
@@ -297,11 +424,15 @@ void app_main(void) {
 			.use_apll = false
 		};
 		ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_0, &i2s_conf, 0, NULL));
+
+		hexdump((unsigned char *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG, sizeof(uint32_t), 8);
 		ESP_ERROR_CHECK(i2s_set_adc_mode(ADC_UNIT_1, ADC1_CHANNEL_0));
 
 		const static uint8_t channelConfig[] = {ADC_CHANNEL_0, ADC_CHANNEL_3,ADC_CHANNEL_4,ADC_CHANNEL_5};
 		// ADC setting
-		struct patternTableEntry *patterns = (struct patternTableEntry *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG;
+		hexdump((unsigned char *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG, sizeof(uint32_t), 8);
+		/*
+		volatile struct patternTableEntry *patterns = (struct patternTableEntry *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG;
 		for (int i = 0; i<4; i++) {
 			patterns[i].atten = ADC_ATTEN_DB_12; //150 mV ~ 2450 mV
 			patterns[i].ch_sel = channelConfig[i];
@@ -311,6 +442,34 @@ void app_main(void) {
 		if (config.channels>4){
 			ESP_LOGE(MAIN_TAG, "channels more than 4 not implemented");
 		};
+		*/
+
+		adc_digi_pattern_table_t adc1_pattern[config.channels];
+		adc_digi_config_t dig_cfg = {
+			.format = DIG_ADC_OUTPUT_FORMAT_DEFUALT,
+			.conv_mode = ADC_CONV_SINGLE_UNIT_1,
+		};
+
+		for (int i = 0; i< config.channels; i++) {
+			adc1_pattern[i].atten = DIG_ADC_ATTEN_DEFUALT;
+			adc1_pattern[i].bit_width = DIG_ADC_BIT_WIDTH_DEFUALT;
+			adc1_pattern[i].channel = channelConfig[i];
+			dig_cfg.adc1_pattern_len = config.channels;
+			dig_cfg.adc1_pattern = adc1_pattern;
+			adc_common_gpio_init(ADC_UNIT_1, channelConfig[i]);
+		} 
+		ADC_ENTER_CRITICAL();
+		adc_ll_digi_set_fsm_time(ADC_LL_FSM_RSTB_WAIT_DEFAULT, ADC_LL_FSM_START_WAIT_DEFAULT,
+				ADC_LL_FSM_STANDBY_WAIT_DEFAULT);
+		adc_ll_set_sample_cycle(ADC_LL_SAMPLE_CYCLE_DEFAULT);
+		adc_ll_pwdet_set_cct(ADC_LL_PWDET_CCT_DEFAULT);
+		adc_ll_digi_output_invert(ADC_UNIT_1, ADC_LL_DIGI_DATA_INVERT_DEFAULT(ADC_UNIT_1));
+		adc_ll_digi_output_invert(ADC_UNIT_2, ADC_LL_DIGI_DATA_INVERT_DEFAULT(ADC_UNIT_2));
+		adc_ll_digi_set_clk_div(ADC_LL_DIGI_SAR_CLK_DIV_DEFAULT);
+		adc_digi_controller_reg_set(&dig_cfg);
+		ADC_EXIT_CRITICAL();
+
+		hexdump((unsigned char *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG, sizeof(uint32_t), 8);
 		ESP_ERROR_CHECK(i2s_adc_enable(I2S_NUM_0));
 
 		// delay for I2S bug workaround
@@ -328,6 +487,7 @@ void app_main(void) {
 		memset(readbuffer, 0xff, sizeof(uint8_t)*frameSize);
 		size_t readData;
 		ESP_LOGI(MAIN_TAG, "sending data");
+		hexdump((unsigned char *)APB_CTRL_APB_SARADC_SAR1_PATT_TAB1_REG, sizeof(uint32_t), 8);
 		do{
 			ESP_ERROR_CHECK(i2s_read(I2S_NUM_0, readbuffer, sizeof(uint8_t)*frameSize, &readData, portMAX_DELAY));
 	//		ESP_LOGI(MAIN_TAG, "read %d bytes", readData);
